8161936481baf0046e038647b85218c781c990c7
doehyeonlee <139297484+doehyeonlee@users.noreply.github.com>
2023-07-27 14:59:24 +0900

    Update data_setting.py

--- data_setting.py
+++ data_setting.py
@@ -5,6 +5,11 @@ import os
 
 cnt_list = [-1]
 
+def clean_lines(text):
+    lines = text.split('\n')
+    cleaned_lines = [line.strip() for line in lines if line.strip() != '']
+    return '\n'.join(cleaned_lines)
+
 def parse_song_content(song_content):
     # Split song content into lyrics and metadata sections
     lyric_section, metadata_section = song_content.split('__', 1)
@@ -49,22 +54,39 @@ for file_name in os.listdir(data_folder):
             song_data = parse_song_content(song_content)
             songs_data.append(song_data)
 
+# Slice for simple test
+songs_data = songs_data[:10]
 # Step 2: Convert the list to a DataFrame
 songs_df = pd.DataFrame(songs_data)
 songs_df['borders'] = songs_df['a_lyrics'].apply(ls.segment_borders)
 
-# Create an empty DataFrame for ssms
-ssms = pd.DataFrame({'id': songs_df['id'], 'a_lyrics': songs_df['a_lyrics']})
-ssms_dict = {}
+# Save the DataFrames
+songs_df.to_hdf('song_data1.hdf', key='df', mode='w')
 
-ssms['simstr'] = ssms['a_lyrics'].apply(ls.calculate_str)
-ssms['simhead'] = ssms['a_lyrics'].apply(ls.calculate_head)
-ssms['simtail'] = ssms['a_lyrics'].apply(ls.calculate_tail)
-ssms['simphone'] = ssms['a_lyrics'].apply(ls.calculate_phone)
-ssms['simpos'] = ssms['a_lyrics'].apply(ls.calculate_pos)
-ssms['simw2v'] = ssms['a_lyrics'].apply(ls.calculate_w2v)
-ssms['simsyW'] = ssms['a_lyrics'].apply(ls.calculate_syW)
-ssms['simsyl'] = ls.simsyl(ssms['a_lyrics'])
+# Create an empty DataFrame for ssms
+ssms = pd.DataFrame({'id': songs_df['id'], 'lyrics': songs_df['a_lyrics'].apply(clean_lines)})
+ssms['simstr'] = ssms['lyrics'].apply(ls.calculate_str)
+print('simstr calculation completed')
+ssms['simhead'] = ssms['lyrics'].apply(ls.calculate_head)
+print('simhead calculation completed')
+ssms['simtail'] = ssms['lyrics'].apply(ls.calculate_tail)
+print('simtail calculation completed')
+ssms.to_hdf('ssm_store1.hdf', key='df', mode='w')
+
+ssms['simphone'] = ssms['lyrics'].apply(ls.calculate_phone)
+print('simphone calculation completed')
+ssms['simpos'] = ssms['lyrics'].apply(ls.calculate_pos)
+print('simpos calculation completed')
+ssms.to_hdf('ssm_store1.hdf', key='df', mode='w')
+
+ssms['simw2v'] = ssms['lyrics'].apply(ls.calculate_w2v)
+print('simw2v calculation completed')
+ssms['sims2v'] = ssms['lyrics'].apply(ls.calculate_s2v)
+print('sims2v calculation completed')
+ssms['simsyW'] = ssms['lyrics'].apply(ls.calculate_syW)
+print('simsyW calculation completed')
+ssms['simsyl'] = ssms['lyrics'].apply(ls.calculate_syl)
+print('simsyl calculation completed')
 
 # Save the DataFrames
 songs_df.to_hdf('song_data1.hdf', key='df', mode='w')
@@ -82,10 +104,12 @@ segm_borders = song.borders
 print('segment borders:', segm_borders, '\n')
 print(ls.pretty_print_tree(ls.tree_structure(ls.normalize_lyric(lyric))))
 
-# #get borders and SSM from stores
-ssm_lines_string = ssms_string[ssms_string['id'] == song_id].iloc[5]['ssm']
-ssm_lines_string = ssms_string[ssms_string['id'] == song_id].iloc[3]['ssm']
+for i in range(9):
+    print(ssms_string.iloc[i].shape)
 
-ssm_drawing.draw_ssm_encodings_side_by_side(ssm_some_encoding=ssm_lines_string, ssm_other_encoding=ssm_lines_string, ssm_third_encoding=ssm_lines_string,\
-                                     representation_some = 'string', representation_other = 'string', representation_third = 'string',\
-                                     artist_name=song.a_name, song_name=song.a_song, genre_of_song='undef')
+# #get borders and SSM from stores
+for i in range(9):
+    ssm_lines_string = ssms_string[ssms_string['id'] == song_id].iloc[i]['ssm']
+    ssm_drawing.draw_ssm_encodings_side_by_side(ssm_some_encoding=ssm_lines_string, ssm_other_encoding=ssm_lines_string, ssm_third_encoding=ssm_lines_string,\
+                                         representation_some = 'string', representation_other = 'string', representation_third = 'string',\
+                                         artist_name=song.a_name, song_name=song.a_song, genre_of_song='undef')
