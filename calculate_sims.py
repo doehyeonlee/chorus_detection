c89667e24b6078df7356435046576b724b5f7667
doehyeonlee <139297484+doehyeonlee@users.noreply.github.com>
2023-07-27 14:55:39 +0900

    Update calculate_sims.py

--- calculate_sims.py
+++ calculate_sims.py
@@ -6,25 +6,20 @@ from scipy.spatial.distance import cosine, euclidean
 from fastdtw import fastdtw
 from gensim.models import Word2Vec, KeyedVectors
 from Levenshtein import distance as lsd
-import phonetics
-import pyphen
+from sentence_transformers import SentenceTransformer
+from hyphenate import hyphenate_word
 
-# Define paths for saved models
+# word2vec model download
 word2vec_model_path = 'saved_word2vec_model.model'
-# context2vec_model_path = 'saved_context2vec_model.model'
-
-# Check if the models exist
 if os.path.exists(word2vec_model_path):
     word2vec_model = KeyedVectors.load(word2vec_model_path)
 else:
     word2vec_model = KeyedVectors.load_word2vec_format("wiki-news-300d-1M-subword.vec", binary=False)
     word2vec_model.save(word2vec_model_path)
 
-# if os.path.exists(context2vec_model_path):
-#     context2vec_model = Word2Vec.load(context2vec_model_path)
-# else:
-#     context2vec_model = Word2Vec.load('context2vec')
-#     context2vec_model.save(context2vec_model_path)
+# sen2vec model download
+sen2vec_model = SentenceTransformer('sentence-transformers/sentence-t5-base')
+
 
 def load_pronunciation_dict(file_path):
     """Load Pronunciation Dictionary."""
@@ -48,14 +43,21 @@ def load_pronunciation_dict(file_path):
 
     return pronunciation_dict
 
+
 cmu_dict = load_pronunciation_dict('model/cmudict.txt')
 download('punkt')
 download('averaged_perceptron_tagger')
 
 def simstr(line1, line2):
     """String similarity using normalized Levenshtein edit distance."""
-    return lsd(line1, line2)
+    try:
+        # Actually, just computing the Normalized Levenshtein edit distance
+        return lsd(line1, line2) / max(len(line1), len(line2))
+    except ZeroDivisionError:
+        print(line1, ":", len(line1), line2, ":", len(line2))
 
+        # If both strings are empty, they are considered similar
+        return 1 if not line1 and not line2 else 0
 
 def simhead(line1, line2):
     """Head similarity."""
@@ -71,11 +73,11 @@ def simtail(line1, line2):
 
 def simphone(line1, line2):
     """Phonetic similarity."""
-
+    line1 = line1.upper()
+    line2 = line2.upper()
     def get_phonetic(line):
         words = word_tokenize(line)
         return [cmu_dict.get(word, [''])[0] for word in words]
-
     return simstr(' '.join(get_phonetic(line1)), ' '.join(get_phonetic(line2)))
 
 
@@ -87,7 +89,6 @@ def simpos(line1, line2):
 
 def simw2v(line1, line2):
     """Word vector similarity."""
-
     def get_avg_vector(line, model):
         vectors = [model[word] for word in word_tokenize(line) if word in model.key_to_index]
         return np.mean(vectors, axis=0) if vectors else np.zeros(model.vector_size)
@@ -95,51 +96,44 @@ def simw2v(line1, line2):
     vec1, vec2 = get_avg_vector(line1, word2vec_model), get_avg_vector(line2, word2vec_model)
     return 1 - cosine(vec1, vec2)
 
-# def simc2v(line1, line2):
-#     """Context vector similarity."""
-#     def get_avg_vector(line, model):
-#         vectors = [model[word] for word in word_tokenize(line) if word in model.vocab]
-#         return np.mean(vectors, axis=0) if vectors else np.zeros(model.vector_size)
-#
-#     vec1, vec2 = get_avg_vector(line1, context2vec_model), get_avg_vector(line2, context2vec_model)
-#     return 1 - cosine(vec1, vec2)
+def sims2v(line1, line2):
+    """Context vector similarity."""
+    vec1, vec2 = sen2vec_model.encode(line1), sen2vec_model.encode(line2)
+    return 1 - cosine(vec1, vec2)
 
-dic = pyphen.Pyphen(lang='en')
+
+def get_syllable_count(word):
+    """Returns the syllable count for a word."""
+    return len(hyphenate_word(word))
 
 def count_syllables(line):
     """Counts the syllables in a line."""
     words = word_tokenize(line)
-    return sum([len(dic.inserted(word).split('-')) for word in words])
-
-def get_syllable_count(word):
-    """Returns the syllable count for a word."""
-    return len(dic.inserted(word).split('-'))
+    return sum([len(hyphenate_word(word)) for word in words])
 
 
 def simsyW(line1, line2):
     """Word syllable count similarity using DTW."""
     syllables1 = [get_syllable_count(word) for word in word_tokenize(line1)]
     syllables2 = [get_syllable_count(word) for word in word_tokenize(line2)]
-    print(syllables1)
-    print(type(syllables1))
-    print(syllables2)
     distance, _ = fastdtw(syllables1, syllables2)
     return 1 / (1 + distance)
 
-def sliding_window(lyrics, window_size=4):
-    for i in range(len(lyrics) - window_size + 1):
-        yield lyrics[i:i + window_size]
 
 def simsyl(lyrics):
-    # Split lyrics into lines and count syllables for each line
+    lyrics = str(lyrics)
     lines = lyrics.split("\n")
-    syllable_counts = [count_syllables(line) for line in lines]
+    # Calculate the total syllable count for each lyric line
+    syllable_counts = [count_syllables(line) for line in lines if count_syllables(line)>0]
+    T = len(syllable_counts)
+    # Create a matrix to store the similarities
+    similarity_matrix = np.zeros((T, T))
 
-    similarities = []
-
-    for win1 in sliding_window(syllable_counts):
-        for win2 in sliding_window(syllable_counts):
-            distance, _ = fastdtw(win1, win2)
-            similarities.append((1 / (1 + distance)))  # Similarity is the inverse of distance
-
-    return np.mean(similarities)
+    # Calculate the similarity using DTW and fill in the matrix
+    for i in range(T):
+        for j in range(T):
+            window1 = syllable_counts[i:i+4]
+            window2 = syllable_counts[j:j+4]
+            distance, _ = fastdtw(window1, window2)
+            similarity_matrix[i, j] = 1 / (1 + distance)  # Similarity is the inverse of distance
+    return similarity_matrix
